6b1204b1dca725a5ef019d3c68bbad4a62a962f0
christian_santana <crsantanarj@hotmail.com>
2023-07-05 21:16:52 -0300

    feat:Criação de modal V1

--- src/Components/FormCreateReview/index.tsx
+++ src/Components/FormCreateReview/index.tsx
@@ -1,26 +1,30 @@
 import { useContext } from "react";
 import { MoviesContext } from "../../providers/MoviesContext/MovieContext";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { TCreateReviews } from "./formCreateReview";
+import { TCreateReviews, formCreateReviewSchema } from "./formCreateReview";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { IReview } from "../../providers/MoviesContext/@types";
 // import { SubmitHandler, useForm } from 'react-hook-form';
 // import { Select } from "../../Fragments/Select";
 
 export const FormCreateReview = () => {
-    const { isOpen, setIsOpen, createReview } = useContext(MoviesContext);
-
-    const { register, handleSubmit, formState: {errors} } = useForm<TCreateReview>({
+    const { createReview } = useContext(MoviesContext);
+  
+    const { register, handleSubmit, formState: {errors} } = useForm<TCreateReviews>({
         resolver: zodResolver(formCreateReviewSchema)
     });
 
-    // const submit: SubmitHandler<TCreateReviews> async (formData: TCreateReview) => {
-    //     createReview()
-    // }
+    const submit: SubmitHandler<TCreateReviews> = async (formData: IReview) => {
+        createReview(formData)
+        console.log(formData)
+    }
+    
 
     return (
         <>
             <h2>Avaliação</h2>
             <form onSubmit={handleSubmit(submit)}>
-                <select {...register('score')} error={errors.score}>
+                <select {...register('score')} onError={errors.score}>
                     <option value="">Selecione uma nota</option>
                     <option value="0">0</option>
                     <option value="1">1</option>
--- src/components/Section/MoviesDetailsSection/MoviesDetailsList/index.tsx
+++ src/components/Section/MoviesDetailsSection/MoviesDetailsList/index.tsx
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import { MoviesContext } from "../../../../providers/MoviesContext/MovieContext";
 import { ReviewsSection } from "../../ReviewsSection";
 import estrela from "../../../../assets/estrela.svg";
+import { Modal } from "../../../Modal";
+import { FormCreateReview } from "../../../FormCreateReview";
 
 export const MoviesDetailsList = () => {
   const { moviesDetails } = useContext(MoviesContext);
+  const { isOpen, setIsOpen, createReview } = useContext(MoviesContext);
 
   if (!moviesDetails) {
     return <div>Carregando detalhes do filme...</div>;
@@ -46,9 +49,15 @@ export const MoviesDetailsList = () => {
       <section>
         <div>
           <h1>AVALIAÇÕES</h1>
-          <button>
+          <button onClick={() => setIsOpen(true)}>
             <img src={estrela} alt="" /> Avaliar
           </button>
+          {isOpen ? (
+            <Modal>
+              <h1>Avaliação</h1>
+              <FormCreateReview />
+            </Modal>) : null}
+
         </div>
         <ReviewsSection />
       </section>
