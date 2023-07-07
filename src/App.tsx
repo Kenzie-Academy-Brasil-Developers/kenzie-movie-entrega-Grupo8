abe9fb6be9eda873147fa50051f37f699d1b43d8
Alando S Almeida Sobrinho <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-07-06 22:36:06 -0300

    fix:app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './index.css'
+import "./index.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { MoviesProvider } from "./providers/MoviesContext/MovieContext";
@@ -7,16 +7,19 @@ import { RoutesMain } from "./routes/RoutesMain";
 export const App = () => {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-      {/* <MoviesProvider>
-        <Header to1={"/register"} text1={"Cadastre-se"} to2={"/login"} text2={"Entrar"} userLogout={function (): void {
-          throw new Error("Function not implemented.");
-        } }/>
+      <MoviesProvider>
+        <Header
+          to1={"/register"}
+          text1={"Cadastre-se"}
+          to2={"/login"}
+          text2={"Entrar"}
+          userLogout={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
         <RoutesMain />
-        <Footer/>
-      </MoviesProvider> */}
+        <Footer />
+      </MoviesProvider>
     </>
   );
 };
