70f99613b4f070ff31e4c536f3c6ad218de367ae
lucas costa <lucascosta120903@gmail.com>
2023-07-07 17:19:34 -0300

    style:desingHomeV2

--- src/Components/HomeSection/index.tsx
+++ src/Components/HomeSection/index.tsx
@@ -2,7 +2,7 @@ import { HomeList } from "./HomeList/index";
 
 export const HomeSection = () => {
   return (
-    <section className="bg-gray-800 w-10/12 flex items-center justify-center m-auto">
+    <section className="bg-transparent w-10/12 flex items-center justify-center m-auto">
         <HomeList />
     </section>
   );
