5458917d06b8c2ee80046bbd2bc0af7668fe0bbe
Alando S Almeida Sobrinho <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-07-07 14:50:43 -0300

    fix: correção

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -10,44 +10,36 @@ interface HeaderProps {
   text2: string;
 }
 
-export const Header = ({}: HeaderProps) => {
-  const { userLogout, firstLetter, user } = useContext(UserContext);
+export const Header = ({ userLogout, to1, text1, to2, text2 }: HeaderProps) => {
+  const { firstLetter, user } = useContext(UserContext);
 
   return (
-    <header className="w-full h-103 p-5 bg-slate-800 flex flex-row items-center z-10 ">
-      <div className="w-10/12 maxh-43 mx-auto bg-yellow-500 flex flex-row gap-4 flex-wrap justify-between items-center">
-        <div className="border-2 border-solid border-gray-400">
-          <h2 className="w-full text-white">kenziemovie</h2>
-        </div>
-        <div className="w-273 h-43 ">
-          {user?.name ? (
-            <>
-              <div className="w-full">
-                <div className="flex items-center justify-between">
-                  <span className="text-white font-poppins text-17 font-semibold flex items-center justify-center w-43 h-43 rounded-full bg-yellow-500">
-                    {firstLetter}
-                  </span>
-                  <span className="text-white font-poppins text-17 font-normal leading-normal color: '#FFF'">
-                    {user?.name}
-                  </span>
-                </div>
+    <header className="w-full bg-slate-800">
+      <div className="container mx-auto max-w-1320">
+        <div className="flex items-center justify-between h-103 p-5">
+          <div className="border-2 border-solid border-gray-400">
+            <h2 className="text-white">kenziemovie</h2>
+          </div>
+          <div className="w-273 h-43">
+            {user?.name ? (
+              <div className="flex items-center justify-between">
+                <span className="text-white font-poppins text-17 font-semibold flex items-center justify-center w-43 h-43 rounded-full bg-yellow-500">
+                  {firstLetter}
+                </span>
+                <span className="text-white font-poppins text-17 font-normal leading-normal">
+                  {user?.name}
+                </span>
                 <NavLink onClick={userLogout} to={""}>
                   Sair
                 </NavLink>
               </div>
-            </>
-          ) : (
-            <>
-              <div className="text-white flex flex-row gap-4">
-                <NavLink to="/register" >
-                  Register
-                </NavLink>
-                <NavLink to="/login" className="text-white">
-                  Login
-                </NavLink>
+            ) : (
+              <div className="text-white flex gap-4">
+                <NavLink to="/register">{text1}</NavLink>
+                <NavLink to="/login">{text2}</NavLink>
               </div>
-            </>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </header>
