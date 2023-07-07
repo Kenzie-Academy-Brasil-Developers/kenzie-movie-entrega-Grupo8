23f1eeb6c76d2bfec6b73b96f683874791b861b7
Alando S Almeida Sobrinho <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-07-07 17:35:23 -0300

    style: header footer MoviesDetails

--- src/components/Section/ReviewsSection/ReviewsCard/index.tsx
+++ src/components/Section/ReviewsSection/ReviewsCard/index.tsx
@@ -1,10 +1,24 @@
+import { useContext } from "react";
 import { IReview } from "../../../../providers/MoviesContext/@types";
+import { UserContext } from "../../../../providers/UserContext/UserContext";
+import estrela from "../../../../assets/estrela.svg";
 
 export const ReviewsCard = ({ review }: { review: IReview }) => {
+  const { firstLetter } = useContext(UserContext);
   return (
-    <ul>
-      <li>{review.score}</li>
-      <li>{review.description}</li>
+    <ul className="w-413 h-510">
+      <li className="w-43 h-43 rounded-full bg-yellow-500">
+        <span className="text-white font-poppins text-17 font-semibold  w-43 h-43">
+          {firstLetter}
+        </span>
+      </li>
+      <li className="flex items-center justify-center">
+        <img src={estrela} alt="estrela da avaliação" />
+        {review.score}
+      </li>
+      <li className="w-320">
+        <p className="font-family-inter text-base">{review.description}</p>
+      </li>
       <li>{review.userName}</li>
     </ul>
   );
