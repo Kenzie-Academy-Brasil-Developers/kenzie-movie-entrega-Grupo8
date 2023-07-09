468c4e27e4c0f44e2732e41147c79c671d487025
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-07-08 22:43:14 -0300

    Estilizando Formulário de Registro

--- src/Components/FormRegister/index.tsx
+++ src/Components/FormRegister/index.tsx
@@ -16,42 +16,75 @@ export const FormRegister = () => {
     }
     
     return (
-      <>
-        <form onSubmit={handleSubmit(submit)} noValidate>
-        <h2>Cadastro</h2>
+      <div className="container w-full flex justify-center sm:px-1 lg:px-1 max-w-[1320px]">
+        <form className="bg-transparent w-full h-auto  py-2 px-2 shandow rounded-lg max-w-[900px]" onSubmit={handleSubmit(submit)} noValidate>
 
-        <p>Preencha os campos para cadastrar-se</p>
+        <div className="flex justify-between items-center">
+          <h2 className='text-white mb-1 font-bold text-5xl ms-2.5'>Cadastro</h2>
 
-        <Input
-        type="name"
-        placeholder="Digite aqui seu Nome"
-        {...register("name")}
-        error={errors.name}
-        />
-       <Input
+          <div className="flex flex-row gap-3 h-4 items-center mt-6 mr-3.5">
+            <img className="mb-3" src="./src/assets/Vector.png" alt="Seta-Vector"/>
+
+            <p className="text-yellow-400 flex justify-center font-bold text-lg mb-4">Voltar</p>
+          </div>   
+        </div>
         
-        type="email"
-        placeholder="Digite aqui seu e-mail"
-        {...register("email")}
-        error={errors.email}
-        />
-        <Input
+        <p className='flex mt-5 font-normal text-base mb-3 ms-2'>Preencha os campos para cadastrar-se</p>
+
+        <div className="flex flex-wrap w-full">
+
+          <div className="w-full md:w-1/2 p-2">
+            <Input
+
+            type="name"
+            placeholder="Digite aqui seu Nome"
+            {...register("name")}
+            error={errors.name}
+            />
+
+          </div>
+         
+          <div className="w-full md:w-1/2 p-2">
+            <Input
+          
+            type="email"
+            placeholder="Digite aqui seu e-mail"
+            {...register("email")}
+            error={errors.email}
+            />
+
+          </div>
+          
+          <div className="w-full md:w-1/2 p-2">
+            <Input
+          
+            type="password"
+            placeholder="Digite aqui sua senha"
+            {...register("password")}
+            error={errors.password}
+            />
+
+          </div>
+          
+          <div className="w-full md:w-1/2 p-2">
+            <Input
         
-        type="password"
-        placeholder="Digite aqui sua senha"
-        {...register("password")}
-        error={errors.password}
-        />
-        <Input
-        
-        type="password"
-        placeholder="Confirme sua senha"
-        {...register("confirm")}
-        error={errors.password}
-        />
-        <button type='submit'>Entrar</button>
+            type="password"
+            placeholder="Confirme sua senha"
+            {...register("confirm")}
+            error={errors.password}
+            />
+
+          </div>
+          
+        </div>
+      
+        <div className="flex justify-end mr-2">
+          <button className='mt-2 bg-yellow-400 text-black inline-block w-46 px-6 py-3.5 leading-none hover:bg-yellow-500 rounded-4xl font-bold text-lg' type='submit'>Cadastrar-se</button>
+        </div>
+
       </form>
-    </>
+    </div>
   );
 };
 
--- src/Fragments/Image/index.tsx
+++ src/Fragments/Image/index.tsx
@@ -5,8 +5,8 @@ interface IbackgroundImageProps {
 export const BackgroundImage = ({children}: IbackgroundImageProps) => {
     return(
         <div className="flex flex-col justify-center bg-no-repeat overflow-y:hidden h-[97vh] bg-fixed"
-            style={{backgroundImage:"url('./src/assets/Rectangle13.png')"}}
-        >
+            style={{backgroundImage:"url('./src/assets/Rectangle13.svg')"}}
+        >       
             {children}
         </div>
     )
